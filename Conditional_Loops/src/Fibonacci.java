0827a9e8cd87f016563c2b048cd5921ef2f3e632
VISHNU-2003 <avishnuvardhan5@gmail.com>
2024-01-03 15:19:37 +0530

    Day1

---
+++ Conditional_Loops/src/Fibonacci.java
@@ -0,0 +1,16 @@
+import  java.util.Scanner;
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a = 0;
+        int b = 1;
+        // with for loop
+        for(int i=2;i<=n;i++){
+            int temp = b;
+            b = b+a;
+             a = temp;
+        }
+        System.out.println(b);
+    }
+}
---
+++ Conditional_Loops/src/whileFibo.java
@@ -0,0 +1,15 @@
+public class whileFibo {
+    public static void main(String[] args) {
+        int n = 8;
+        int a = 0;
+        int b = 1;
+        int i = 2;
+        while(i<=n){
+            int temp = b;
+            b = b+a;
+            a = temp;
+            i++;
+        }
+        System.out.println(b);
+    }
+}
